1bef26d9443e7f865ed3148e85fd68c6e1cf3292
Djontleman <jhutchinson531@gmail.com>
2021-10-22 11:36:08 +0100

    Complete Person class

--- src/main/java/spaceship/Person.java
+++ src/main/java/spaceship/Person.java
@@ -17,5 +17,66 @@ public class Person {
         this.passportNumber = passportNumber;
     }
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Passport getPassport() {
+        return passport;
+    }
+
+    public void setPassport(Passport passport) {
+        this.passport = passport;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", passport=" + passport +
+                '}';
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Person person = (Person) object;
+        return phoneNumber == person.phoneNumber && java.util.Objects.equals(id, person.id) && java.util.Objects.equals(name, person.name) && java.util.Objects.equals(email, person.email) && java.util.Objects.equals(passport, person.passport);
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, name, email, phoneNumber, passport);
+    }
 }
