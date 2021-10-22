c268c1205f47519da8d895bc4d3616b72eb7df7c
Vinh Chu <vinhchugg@gmail.com>
2021-10-22 10:53:48 +0100

    create file structure

---
+++ src/main/java/spaceship/BookingSystem.java
@@ -0,0 +1,4 @@
+package spaceship;
+
+public class BookingSystem {
+}
---
+++ src/main/java/spaceship/Main.java
@@ -0,0 +1,12 @@
+package spaceship;
+
+//     Display the list of available options such as:
+//        Option 1 Add new Flight
+//        Option 2 Display all flights
+//        Option 3 Display Booked Flights
+//        Option 4 Display flight for a specific user
+//        Option 5 Book a flight for a user using his id
+//        Option 6 Cancel a flight
+
+public class Main {
+}
