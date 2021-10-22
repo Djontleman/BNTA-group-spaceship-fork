45283c02c985380452a7f6e6cc7add39b344020e
TamaraHerrington <tamaraherrington24@gmail.com>
2021-10-22 12:36:29 +0100

    create addPerson method to SpaceshipService.java.

--- src/main/java/spaceship/SpaceshipService.java
+++ src/main/java/spaceship/SpaceshipService.java
@@ -1,5 +1,17 @@
 package spaceship;
 
-public class SpaceshipService {
+import spaceship.person.Person;
 
+public class SpaceshipService {
+    //method to add new person
+    public void addPerson (Spaceship spaceship1, Person person1){
+        if (spaceship1.getCount() < spaceship1.getCapacity()){
+            Person[] passengers = spaceship1.getPassengers();
+            passengers[spaceship1.getCount()] = person1;
+            spaceship1.setCount(spaceship1.getCount()+1);
+            System.out.println("Welcome aboard!");
+        } else {
+            System.out.println("Sorry, we're full");
+        }
+    }
 }
