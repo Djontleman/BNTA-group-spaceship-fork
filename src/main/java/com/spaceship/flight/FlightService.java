ce1e172327ee54540b56cd9e215274d029a4a531
Djontleman <jhutchinson531@gmail.com>
2021-10-24 18:05:46 +0100

    Move addPerson method from SpaceshipService to FlightService

--- src/main/java/com/spaceship/flight/FlightService.java
+++ src/main/java/com/spaceship/flight/FlightService.java
@@ -1,5 +1,6 @@
 package com.spaceship.flight;
 
+import com.spaceship.person.Person;
 import com.spaceship.spaceship.Spaceship;
 
 public class FlightService {
@@ -8,6 +9,18 @@ public class FlightService {
 
     public void addFlight(Flight spaceship) {
 
+    }
+
+    // Method to add new person to flight
+    public void addPerson (Flight flight1, Person person1){
+        if (flight1.getCount() < flight1.getCapacity()){
+            Person[] passengers = flight1.getPassengers();
+            passengers[flight1.getCount()] = person1;
+            flight1.setCount(flight1.getCount()+1);
+            System.out.println("Welcome aboard!");
+        } else {
+            System.out.println("Sorry, we're full");
+        }
     }
 
 
--- src/main/java/com/spaceship/spaceship/SpaceshipService.java
+++ src/main/java/com/spaceship/spaceship/SpaceshipService.java
@@ -5,17 +5,6 @@ import com.spaceship.person.Person;
 import java.util.Arrays;
 
 public class SpaceshipService {
-    //method to add new person
-//    public void addPerson (Spaceship spaceship1, Person person1){
-//        if (spaceship1.getCount() < spaceship1.getCapacity()){
-//            Person[] passengers = spaceship1.getPassengers();
-//            passengers[spaceship1.getCount()] = person1;
-//            spaceship1.setCount(spaceship1.getCount()+1);
-//            System.out.println("Welcome aboard!");
-//        } else {
-//            System.out.println("Sorry, we're full");
-//        }
-//    }
 
 //    public void removePerson (SpaceshipService spaceship1, Person person1) {
 //        Person[] passengers = spaceship1.getPassengers();
