fb427dd9afdeb7f66b1227bdfce696585d218ae2
Djontleman <jhutchinson531@gmail.com>
2021-10-24 15:32:09 +0100

    Update Flight class to work with new Spaceship class

--- src/main/java/com/spaceship/flight/Flight.java
+++ src/main/java/com/spaceship/flight/Flight.java
@@ -4,18 +4,21 @@ import com.spaceship.person.Person;
 import com.spaceship.spaceship.Spaceship;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Flight {
 
     private Spaceship spaceship;
+    private String spaceshipCallSign;
     private int capacity;
-    private Person[] passengerIDs;
+    private Person[] passengers;
     private FlightJourney flightJourney;
 
     public Flight(Spaceship spaceship, FlightJourney flightJourney) {
         this.spaceship = spaceship;
+        this.spaceshipCallSign = spaceship.getCallSign();
         this.capacity = spaceship.getCapacity();
-        this.passengerIDs = new Person[capacity];
+        this.passengers = new Person[capacity];
         this.flightJourney = flightJourney;
     }
 
@@ -27,6 +30,14 @@ public class Flight {
         this.spaceship = spaceship;
     }
 
+    public String getSpaceshipCallSign() {
+        return spaceshipCallSign;
+    }
+
+    public void setSpaceshipCallSign(String spaceshipCallSign) {
+        this.spaceshipCallSign = spaceshipCallSign;
+    }
+
     public int getCapacity() {
         return capacity;
     }
@@ -35,12 +46,12 @@ public class Flight {
         this.capacity = capacity;
     }
 
-    public Person[] getPassengerIDs() {
-        return passengerIDs;
+    public Person[] getPassengers() {
+        return passengers;
     }
 
-    public void setPassengerIDs(Person[] passengerIDs) {
-        this.passengerIDs = passengerIDs;
+    public void setPassengers(Person[] passengers) {
+        this.passengers = passengers;
     }
 
     public FlightJourney getFlightJourney() {
@@ -55,9 +66,25 @@ public class Flight {
     public String toString() {
         return "Flight{" +
                 "spaceship=" + spaceship +
+                ", spaceshipCallSign='" + spaceshipCallSign + '\'' +
                 ", capacity=" + capacity +
-                ", passengerIDs=" + Arrays.toString(passengerIDs) +
+                ", passengers=" + Arrays.toString(passengers) +
                 ", flightJourney=" + flightJourney +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flight flight = (Flight) o;
+        return capacity == flight.capacity && Objects.equals(spaceship, flight.spaceship) && Objects.equals(spaceshipCallSign, flight.spaceshipCallSign) && Arrays.equals(passengers, flight.passengers) && flightJourney == flight.flightJourney;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(spaceship, spaceshipCallSign, capacity, flightJourney);
+        result = 31 * result + Arrays.hashCode(passengers);
+        return result;
     }
 }
