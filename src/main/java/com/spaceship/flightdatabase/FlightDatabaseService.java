7147463ce17d9b59b8bfd429d07c4461b6c1a825
Djontleman <jhutchinson531@gmail.com>
2021-10-24 17:38:52 +0100

    Create FlightDatabaseService class with initial list of potential methods to be implemented

---
+++ src/main/java/com/spaceship/flightdatabase/FlightDatabaseService.java
@@ -0,0 +1,15 @@
+package com.spaceship.flightdatabase;
+
+public class FlightDatabaseService {
+
+    // List of potential Methods:
+    // - listAllFlights
+
+    // - addFlight
+    // - removeFlight
+
+    // - getOpenFlights
+    // - listOpenFlights
+    // - getFullFlights
+    // - listFullFlights
+}
