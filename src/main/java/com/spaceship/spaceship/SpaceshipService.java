bb0195efbe6c53ed320e82e3d63dabe3f29b22ed
Djontleman <jhutchinson531@gmail.com>
2021-10-24 18:30:02 +0100

    Clean up SpaceshipService and add initial list of potential methods

--- src/main/java/com/spaceship/spaceship/SpaceshipService.java
+++ src/main/java/com/spaceship/spaceship/SpaceshipService.java
@@ -1,12 +1,13 @@
 package com.spaceship.spaceship;
 
-import com.spaceship.person.Person;
-
-import java.util.Arrays;
-
 public class SpaceshipService {
+
+    // List of potential methods:
+    // - updateCapacity (if capacity changes for whatever reason)
+
+    // These methods are not implementable yet
+    // - printStatus (status not implemented)
+    // - updateStatus (i.e. needs servicing)
 
 }
 
-
-//booking a flight fot
