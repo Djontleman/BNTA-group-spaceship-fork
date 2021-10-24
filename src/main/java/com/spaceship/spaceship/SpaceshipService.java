6f60ac5af1d709a2eaf4db599e5b348af6b67126
Djontleman <jhutchinson531@gmail.com>
2021-10-24 18:02:02 +0100

    Comment out methods in SpaceshipService as these methods will be used in other classes

--- src/main/java/com/spaceship/spaceship/SpaceshipService.java
+++ src/main/java/com/spaceship/spaceship/SpaceshipService.java
@@ -6,16 +6,16 @@ import java.util.Arrays;
 
 public class SpaceshipService {
     //method to add new person
-    public void addPerson (Spaceship spaceship1, Person person1){
-        if (spaceship1.getCount() < spaceship1.getCapacity()){
-            Person[] passengers = spaceship1.getPassengers();
-            passengers[spaceship1.getCount()] = person1;
-            spaceship1.setCount(spaceship1.getCount()+1);
-            System.out.println("Welcome aboard!");
-        } else {
-            System.out.println("Sorry, we're full");
-        }
-    }
+//    public void addPerson (Spaceship spaceship1, Person person1){
+//        if (spaceship1.getCount() < spaceship1.getCapacity()){
+//            Person[] passengers = spaceship1.getPassengers();
+//            passengers[spaceship1.getCount()] = person1;
+//            spaceship1.setCount(spaceship1.getCount()+1);
+//            System.out.println("Welcome aboard!");
+//        } else {
+//            System.out.println("Sorry, we're full");
+//        }
+//    }
 
 //    public void removePerson (SpaceshipService spaceship1, Person person1) {
 //        Person[] passengers = spaceship1.getPassengers();
@@ -23,13 +23,13 @@ public class SpaceshipService {
 //        ArrayUtils.remove(passengers, person1);
 //    }
 
-    public static void addFlight() {
+//    public static void addFlight() {
+//
+//    }
 
-    }
-
-    public static void showFlights (Spaceship[] listOfFlights) {
-        System.out.println(Arrays.toString(listOfFlights));
-    }
+//    public static void showFlights (Spaceship[] listOfFlights) {
+//        System.out.println(Arrays.toString(listOfFlights));
+//    }
 }
 
 
